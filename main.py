afc45311d0f438c9b6e48dfbc0fd8b8fa3163626
Shwapsy <63334786+Shwapsy@users.noreply.github.com>
2025-12-24 16:32:02 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from src.bot import run_bot
+import uvicorn
+import os
+from src.bot import app
 
 if __name__ == "__main__":
-    run_bot()
+    port = int(os.environ.get("PORT", 5000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
